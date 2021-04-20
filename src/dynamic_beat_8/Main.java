c3c82c5e38063f3a65cefa68cfc09385648c2b5f
sunyoring <78639649+sunyoring@users.noreply.github.com>
2021-04-20 22:56:21 +0900

    Add files via upload

---
+++ src/dynamic_beat_8/Main.java
@@ -0,0 +1,15 @@
+
+package dynamic_beat_8;
+
+public class Main {
+
+	// 한 번 선언되면 바뀌지 않는 상수의 경우 전부 대문자로 쓴다.
+	public static final int SCREEN_WIDTH = 1280; // 해상도 1280*720
+	public static final int SCREEN_HEIGHT = 720;
+
+	public static void main(String[] args) {
+
+		new DynamicBeat();
+	}
+
+}
